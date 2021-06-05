e72f441a7dd1eef2b403704f2ce8a59bf6e95e2b
takapy <takanobu.030210@gmail.com>
2021-06-05 10:12:40 +0900

    [add]ans_51

---
+++ chapter6/ans_51.py
@@ -0,0 +1,88 @@
+"""
+51. 特徴量抽出
+学習データ，検証データ，評価データから特徴量を抽出し，それぞれtrain.feature.txt，valid.feature.txt，test.feature.txtというファイル名で保存せよ． 
+なお，カテゴリ分類に有用そうな特徴量は各自で自由に設計せよ．記事の見出しを単語列に変換したものが最低限のベースラインとなるであろう．
+"""
+
+import pandas as pd
+import texthero as hero
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def load_data() -> dict:
+    """データの読み込み"""
+    # 読み込むファイルを定義
+    inputs = {
+        'train': 'train.txt',
+        'valid': 'valid.txt',
+        'test': 'test.txt',
+    }
+
+    dfs = {}
+    for k, v in inputs.items():
+        dfs[k] = pd.read_csv(v, sep='\t')
+
+    # データチェック
+    for k in inputs.keys():
+        print(k)
+        print(dfs[k].head())
+
+    return dfs
+
+
+def preprocess(text) -> str:
+    """前処理"""
+    clean_text = hero.clean(text, pipeline=[
+        hero.preprocessing.fillna,
+        hero.preprocessing.lowercase,
+        hero.preprocessing.remove_digits,
+        hero.preprocessing.remove_punctuation,
+        hero.preprocessing.remove_diacritics,
+        hero.preprocessing.remove_stopwords
+    ])
+
+    return clean_text
+
+
+class FeatureExtraction():
+
+    def __init__(self, min_df=1, max_df=1) -> None:
+        self.tfidf_vec = TfidfVectorizer(min_df=min_df, max_df=max_df)
+
+    def fit(self, input_text) -> None:
+        self.tfidf_vec.fit(input_text)
+
+    def transform(self, input_text) -> pd.DataFrame:
+        _tfidf_vec = self.tfidf_vec.transform(input_text)
+        return _tfidf_vec
+
+
+if __name__ == "__main__":
+
+    dfs = load_data()
+
+    # trainとtestを生成
+    train = pd.concat([dfs['train'], dfs['valid']], axis=0).reset_index(drop=True)
+    test = dfs['test']
+
+    # 前処理
+    train['clean_title'] = train[['title']].apply(preprocess)
+    test['clean_title'] = test[['title']].apply(preprocess)
+
+    # 特徴量抽出
+    feat = FeatureExtraction()
+    feat.fit(train['clean_title'])
+    X_train = feat.transform(train['clean_title'])
+    X_test = feat.transform(test['clean_title'])
+
+    # DFに変換
+    X_train = pd.DataFrame(X_train.toarray(), columns=feat.tfidf_vec.get_feature_names())
+    X_test = pd.DataFrame(X_test.toarray(), columns=feat.tfidf_vec.get_feature_names())
+
+    # 分割して保存
+    X_train = X_train[:len(dfs['train'])].reset_index(drop=True)
+    X_valid = X_train[len(dfs['train']):]
+
+    X_train.to_csv('X_train.txt', sep='\t', index=False)
+    X_valid.to_csv('X_valid.txt', sep='\t', index=False)
+    X_test.to_csv('X_test.txt', sep='\t', index=False)
